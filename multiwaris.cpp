05f94d85cd0e1280b22f231fc67eb3dfaccba439
TugasConditionalStatement_0089 <rajhwaa@gmail.com>
2026-05-19 15:24:21 +0700

    library

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -1,12 +1,3 @@
 #include <iostream>
 using namespace std;
 
-class orang{
-public:
-    int umur;
-    orang(int pUmur) :
-        umur(pUmur)
-    {
-        cout << "orang dibuat dengan umur" << umur << "\n" << endl;
-    }
-};
