e2962f6f8ab0bb82c210c8d21baaec994386c758
TugasConditionalStatement_0089 <rajhwaa@gmail.com>
2026-05-19 15:28:02 +0700

    feat: implement 'AbstraksiKlas' with methods for setting and displaying values

--- abstraksi.cpp
+++ abstraksi.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 
 class AbstraksiKlas{
-
+private:
+    string x, y;
+public:
+    //method untuk mengisi nilai
+    //private member
+    void setXY(string a, string b){
+        x = a;
+        y = b;
+    }
+    //menampilkan nilai
+    void display(){
+        cout << "Nilai x : " << x << endl;
+        cout << "Nilai y : " << y << endl;
+    }
 };
