331d377043507dd59819836622d36b27e4ede613
TugasConditionalStatement_0089 <rajhwaa@gmail.com>
2026-05-19 15:37:29 +0700

    feat: add main function to demonstrate 'remoteLampu' class functionality in enkapsulasi.cpp

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -12,3 +12,15 @@ public:
         return SaklarNo[i];
     }
 };
+int main(){
+    remoteLampu LampuRumah;
+    LampuRumah.setSaKlarNo(0, "Lampur Teras Ruma");
+    LampuRumah.setSaKlarNo(1, "Lampu Ruang Tamu");
+    LampuRumah.setSaKlarNo(2, "Lampu Kamar Tidur");
+    LampuRumah.setSaKlarNo(3, "Lampu Dapur");
+    cout << LampuRumah.getSaKlarNo(0) << endl;
+    cout << LampuRumah.getSaKlarNo(1) << endl;
+    cout << LampuRumah.getSaKlarNo(2) << endl;
+    cout << LampuRumah.getSaKlarNo(3) << endl;
+    return 0;
+}
